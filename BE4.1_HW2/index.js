0c9c81c7b6ce742c87dd763108fe33374079c1f4
divyanshu40 <divyanshuprajapati40@gmail.com>
2025-04-10 01:13:02 +0530

    updated vercel.json, index.js, package.json, package-lock.json files

--- BE4.1_HW2/index.js
+++ BE4.1_HW2/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const { initializeDatabase } = require("./db/db.connect");
 const { hotel } = require("./models/hotel.model");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 app.listen(PORT, () => {
     console.log("The server is running");
 });
